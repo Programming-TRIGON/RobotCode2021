706acb1960d3a3258b72b308ad66d5dc4a5d43c0
lycaon7 <65887925+lycaon7@users.noreply.github.com>
2021-02-10 22:27:08 +0200

    Updated target enum

--- src/main/java/frc/robot/vision/Target.java
+++ src/main/java/frc/robot/vision/Target.java
@@ -3,27 +3,20 @@ package frc.robot.vision;
 /**
  * This enum represent a potential targets that the robot
  * can follow using vision.
- * Each of the targets has an index, 
+ * Each of the targets has an index,
  * representing what pipeline limelight should use for finding it.
- * And a double representing the distance that the robot needs to be from the target.
  */
 public enum Target {
-  //These are examples for targets.
-  RocketMiddle(0, 0), RocketSide(1, 3.3), Feeder(2, 0);
+    Tower(0), Ball(1);
 
-  private final int index;
-  private final double distance;
+    private final int index;
 
-  Target(int index, double distance) {
-    this.index = index;
-    this.distance = distance;
-  }
+    Target(int index) {
+        this.index = index;
+    }
 
-  public int getIndex() {
-    return index;
-  }
+    public int getIndex() {
+        return index;
+    }
 
-  public double getDistance() {
-    return distance;
-  }
 }
