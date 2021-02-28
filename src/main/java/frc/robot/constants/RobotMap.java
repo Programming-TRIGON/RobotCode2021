8838008073e574ca14ae41fc0579f7beb048824f
lycaon7 <65887925+lycaon7@users.noreply.github.com>
2021-02-28 17:43:58 +0200

    fix to loaderSS

--- src/main/java/frc/robot/constants/RobotMap.java
+++ src/main/java/frc/robot/constants/RobotMap.java
@@ -22,7 +22,7 @@ public abstract class RobotMap {
 
     public class CAN {
         public DrivetrainMap drivetrainMap = new DrivetrainMap();
-        public LoaderMap triggerMap = new LoaderMap();
+        public LoaderMap loaderMap = new LoaderMap();
         public ShooterMap shooterMap = new ShooterMap();
         public IntakeMap intakeMap = new IntakeMap();
 
