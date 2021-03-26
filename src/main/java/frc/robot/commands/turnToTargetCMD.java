85a5e70ee192aa89a742326067af2daedf18c256
Mboesn <45824003+Mboesn@users.noreply.github.com>
2021-03-26 18:10:32 +0300

    fixed turn to target (#40)

--- src/main/java/frc/robot/commands/turnToTargetCMD.java
+++ src/main/java/frc/robot/commands/turnToTargetCMD.java
@@ -1,42 +1,39 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.constants.RobotConstants;
+import frc.robot.constants.RobotConstants.VisionConstants;
 import frc.robot.vision.limelights.VanillaLimelight;
-import frc.robot.subsystems.MovableSubsystem;
+import frc.robot.subsystems.drivetrain.DrivetrainSS;
 import frc.robot.vision.Target;
-import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.utilities.DriverStationLogger;
 import frc.robot.utilities.PIDCoefs;
+import frc.robot.utilities.TrigonPIDController;
 import edu.wpi.first.wpilibj.Timer;
 
 /**
- * This is just template for a subsystem turn to target command. It will
- * probably be changed according the game and the robot.
+ * Turns the drivetrain to face a given target
  */
-public class GenericTurnToTargetCMD extends CommandBase {
+public class turnToTargetCMD extends CommandBase {
   private VanillaLimelight limelight;
-  private RobotConstants.VisionConstants visionConstants;
-  private MovableSubsystem subsystem;
+  private VisionConstants visionConstants;
+  private DrivetrainSS drivetrain;
   private Target target;
-  private PIDController rotationPIDController;
+  private TrigonPIDController rotationPIDController;
   private double lastTimeSeenTarget;
 
-  public GenericTurnToTargetCMD(VanillaLimelight limelight, RobotConstants.VisionConstants visionConstants, Target target,
-                                MovableSubsystem subsystem) {
+  public turnToTargetCMD(DrivetrainSS drivetrain, VanillaLimelight limelight, VisionConstants visionConstants,
+      Target target) {
 
-    addRequirements(subsystem);
+    addRequirements(drivetrain);
 
     this.limelight = limelight;
     this.visionConstants = visionConstants;
     this.target = target;
-    this.subsystem = subsystem;
+    this.drivetrain = drivetrain;
 
     PIDCoefs rotationSettings = visionConstants.ROTATION_SETTINGS;
-    rotationPIDController = new PIDController(rotationSettings.getKP(), rotationSettings.getKI(),
-        rotationSettings.getKD());
-    rotationPIDController.setTolerance(rotationSettings.getTolerance(), rotationSettings.getDeltaTolerance());
-
+    rotationPIDController = new TrigonPIDController(rotationSettings);
   }
 
   @Override
@@ -51,16 +48,18 @@ public class GenericTurnToTargetCMD extends CommandBase {
   @Override
   public void execute() {
     if (limelight.getTv()) {
-      subsystem.move(rotationPIDController.calculate(limelight.getAngle()));
+      drivetrain.fieldPowerDrive(0, 0, rotationPIDController.calculate(limelight.getAngle()));
       lastTimeSeenTarget = Timer.getFPGATimestamp();
-    } else
+    } else {
       // The target wasn't found
-      subsystem.stopMoving();
+      drivetrain.stopMoving();
+      DriverStationLogger.logToDS("turnToTargetCMD: Target not found!");
+    }
   }
 
   @Override
   public void end(boolean interrupted) {
-    subsystem.stopMoving();
+    drivetrain.stopMoving();
     limelight.stopVision();
   }
 
