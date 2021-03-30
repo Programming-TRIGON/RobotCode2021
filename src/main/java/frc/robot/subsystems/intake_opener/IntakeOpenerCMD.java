c84a04ed95c66dada441a28e8e13a0ebfaf68dba
Mboesn <45824003+Mboesn@users.noreply.github.com>
2021-03-30 19:42:54 +0300

    Binding commands (#41)
    * added binding for commands and fixed file and folder names
    
    * added requirements
    
    * removed unnecessary settings
    
    * fixed requirements

--- src/main/java/frc/robot/subsystems/intake_opener/IntakeOpenerCMD.java
+++ src/main/java/frc/robot/subsystems/intake_opener/IntakeOpenerCMD.java
@@ -1,24 +1,24 @@
-package frc.robot.subsystems.intakeOpener;
+package frc.robot.subsystems.intake_opener;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.constants.RobotConstants.IntakeOpenerConstants;
 
 import java.util.function.DoubleSupplier;
 
-public class intakeOpenerCMD extends CommandBase {
+public class IntakeOpenerCMD extends CommandBase {
     private final IntakeOpenerSS intakeOpenerSS;
     private final IntakeOpenerConstants constants;
     private final DoubleSupplier power;
 
-    public intakeOpenerCMD(IntakeOpenerSS intakeOpenerSS, IntakeOpenerConstants constants, DoubleSupplier power) {
+    public IntakeOpenerCMD(IntakeOpenerSS intakeOpenerSS, IntakeOpenerConstants constants, DoubleSupplier power) {
         this.intakeOpenerSS = intakeOpenerSS;
         this.constants = constants;
         this.power = power;
         addRequirements(this.intakeOpenerSS);
     }
 
-    public intakeOpenerCMD(IntakeOpenerSS intakeOpenerSS, IntakeOpenerConstants constants) {
-        this(intakeOpenerSS, constants, () -> constants.DEFAULT_MOTOR_POWER);
+    public IntakeOpenerCMD(IntakeOpenerSS intakeOpenerSS, IntakeOpenerConstants constants) {
+        this(intakeOpenerSS, constants, () -> constants.DEFAULT_OPEN_POWER);
     }
 
     @Override
