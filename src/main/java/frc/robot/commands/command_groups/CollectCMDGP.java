2c294ff825e2adb09d886c0b412853f4d8b6a7a9
lycaon7 <65887925+lycaon7@users.noreply.github.com>
2021-05-27 20:47:01 +0300

    Changed intake opener to solenoid

--- src/main/java/frc/robot/commands/command_groups/CollectCMDGP.java
+++ src/main/java/frc/robot/commands/command_groups/CollectCMDGP.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.command_groups;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.SubsytemContainer;
@@ -23,7 +24,7 @@ public class CollectCMDGP extends SequentialCommandGroup {
     }
 
     private void addCommandsToGroup() {
-        addCommands(new IntakeOpenerCMD(subsystems.INTAKE_OPENER_SS, constants.intakeOpenerConstants),
+        addCommands(new InstantCommand(subsystems.INTAKE_OPENER_SS::toggleSolenoid, subsystems.INTAKE_OPENER_SS),
                 new ParallelCommandGroup(
                         new LoaderCMD(subsystems.LOADER_SS, constants.loaderConstants,
                                 constants.loaderConstants.DEFAULT_MIXING_VELOCITY),
--- src/main/java/frc/robot/subsystems/intake_opener/IntakeOpenerSS.java
+++ src/main/java/frc/robot/subsystems/intake_opener/IntakeOpenerSS.java
@@ -1,59 +1,41 @@
 package frc.robot.subsystems.intake_opener;
 
-import edu.wpi.first.wpilibj.DigitalInput;
-import frc.robot.components.TrigonTalonSRX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.components.TrigonDoubleSolenoid;
 import frc.robot.constants.RobotConstants.IntakeOpenerConstants;
-import frc.robot.subsystems.OverridableSubsystem;
-import frc.robot.utilities.DriverStationLogger;
 import io.github.oblarg.oblog.Loggable;
 import io.github.oblarg.oblog.annotations.Log;
 
-public class IntakeOpenerSS extends OverridableSubsystem implements Loggable {
-    private final TrigonTalonSRX motor;
-    private final DigitalInput closedSwitch;
-    private final DigitalInput openSwitch;
+public class IntakeOpenerSS extends SubsystemBase implements Loggable {
+    private final TrigonDoubleSolenoid solenoid;
     private final IntakeOpenerConstants constants;
 
     public IntakeOpenerSS(IntakeOpenerConstants constants) {
-        motor = constants.CAN_MAP.MOTOR;
-        closedSwitch = constants.DIO_MAP.CLOSED_SWITCH;
-        openSwitch = constants.DIO_MAP.OPEN_SWITCH;
+        solenoid = constants.PCM_MAP.SOLENOID;
         this.constants = constants;
     }
 
-    @Override
-    public void overriddenMove(double power) {
-        motor.set(power);
-    }
-
     /**
-     * @return is the intake closed
-     */
-    @Log(name = "Intake closed")
-    public boolean isClosed() {
-        return closedSwitch.get();
-    }
-
-    /**
-     * @return is the intake open
-     */
-    @Log(name = "Intake open")
-    public boolean isOpen() {
-        return openSwitch.get();
-    }
-
-    /**
-     * Sets the motor power only if the power given is appropriate
+     * Sets the state of the rightSolenoid with a boolean
      *
-     * @param power to be set to the motor
+     * @param state to be set to the rightSolenoid (true=forward false=reverse)
      */
-    public void moveWithSafety(double power) {
-        if (isClosed() && power < 0 || isOpen() && power > 0) {
-            DriverStationLogger.logToDS("IntakeOpener: moveWithSafety: invalid power given");
-            motor.stopMotor();
-        }
-        else
-            motor.set(power);
+    public void setSolenoidState(boolean state) {
+        solenoid.setSolenoid(state);
+    }
+
+    /**
+     * Gets the state of the rightSolenoid as a boolean
+     *
+     * @return the state of the rightSolenoid (true=forward false=reverse)
+     */
+    @Log(name = "Is open")
+    public boolean getSolenoidState() {
+        return solenoid.isForward();
+    }
+
+    public void toggleSolenoid() {
+        setSolenoidState(!getSolenoidState());
     }
 
     @Override
