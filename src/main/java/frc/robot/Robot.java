dec53d10f0f645d8124e42e6b2ddcc642f670af4
Mboesn <moshechelsky@gmail.com>
2021-06-20 23:03:30 +0300

    made robot nicer

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,50 +4,51 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
-    private RobotContainer robotContainer;
+	private RobotContainer robotContainer;
 
-    @Override
-    public void robotInit() {
-        robotContainer = new RobotContainer();
-    }
+	@Override
+	public void robotInit() {
+		robotContainer = new RobotContainer();
+		// robot.win=true
+	}
 
-  @Override
-  public void robotPeriodic() {
-    robotContainer.periodic();
-    CommandScheduler.getInstance().run();
-  }
+	@Override
+	public void robotPeriodic() {
+		robotContainer.periodic();
+		CommandScheduler.getInstance().run();
+	}
 
-  @Override
-  public void autonomousInit() {
-    robotContainer.autonomousInit();
-  }
+	@Override
+	public void autonomousInit() {
+		robotContainer.autonomousInit();
+	}
 
-    @Override
-    public void autonomousPeriodic() {
-    }
+	@Override
+	public void autonomousPeriodic() {
+	}
 
-  @Override
-  public void teleopInit() {
-    robotContainer.teleopInit();
-  }
+	@Override
+	public void teleopInit() {
+		robotContainer.teleopInit();
+	}
 
-    @Override
-    public void teleopPeriodic() {
-    }
+	@Override
+	public void teleopPeriodic() {
+	}
 
-    @Override
-    public void disabledInit() {
-    }
+	@Override
+	public void disabledInit() {
+	}
 
-    @Override
-    public void disabledPeriodic() {
-    }
+	@Override
+	public void disabledPeriodic() {
+	}
 
-    @Override
-    public void testInit() {
-    }
+	@Override
+	public void testInit() {
+	}
 
-    @Override
-    public void testPeriodic() {
-    }
+	@Override
+	public void testPeriodic() {
+	}
 }
